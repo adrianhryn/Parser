e0a41d2f6e2870e3e3282485a9612a7ea7417e99
adrianhryn <36327238+adrianhryn@users.noreply.github.com>
2018-12-23 11:13:11 +0200

    Update City.java

--- City.java
+++ City.java
@@ -12,10 +12,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.io.IOException;
-import java.util.concurrent.Executors;
-
 
 @Getter@Setter@ToString
 public class City {
